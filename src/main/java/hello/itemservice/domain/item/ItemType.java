1d24eb4b43b4a68b3823fbec9744513bff0b2341
YooHyeok <yjk7454@naver.com>
2023-04-03 14:59:53 +0900

    DisplayCode.java - 배송방식Dto클래스 생성

--- src/main/java/hello/itemservice/domain/item/ItemType.java
+++ src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -1,10 +1,10 @@
 package hello.itemservice.domain.item;
 
 public enum ItemType {
-
+    // (괄호)안의 description은 Enum의 String field 이다.
     BOOK("도서"), FOOD("음식"), ETC("기타");
 
-    private final String description; // Enum의 String field
+    private final String description;
 
     ItemType(String description) { // 생성자 Enum타입
         this.description = description;
