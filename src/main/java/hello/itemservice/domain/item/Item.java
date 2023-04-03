01510455ce3920ae377e04d7d22ba635af7630ed
YooHyeok <yjk7454@naver.com>
2023-04-03 15:04:03 +0900

    Item.java - 체크박스, 라디오박스, 콤보박스를 위한 필드 추가

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,6 +2,8 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Item {
 
@@ -9,6 +11,12 @@ public class Item {
     private String itemName;
     private Integer price;
     private Integer quantity;
+
+    // MVC-part02 : 체크박스, 라디오박스, 콤보박스를 위한 필드 추가.
+    private Boolean open; //판매 여부
+    private List<String> regions; //등록 지역
+    private ItemType itemType; //상품 종류 (Enum Type)
+    private String deliveryCode; //배송 방식
 
     public Item() {
     }
