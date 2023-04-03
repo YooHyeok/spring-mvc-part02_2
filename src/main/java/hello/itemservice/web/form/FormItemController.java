8fe02f836e3be44b229d0b7c6b518b0e0456ea35
YooHyeok <yjk7454@naver.com>
2023-04-03 18:20:00 +0900

    item.html - 상품종류 radio Button 추가

--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -61,6 +61,7 @@ public class FormItemController {
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
         log.info("item.open={}",item.getOpen());
         log.info("item.regions={}",item.getRegions());
+        log.info("item.itemType={}",item.getItemType());
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
